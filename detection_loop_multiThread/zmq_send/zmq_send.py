92b1caaa2d12f203f8a9077390ca6ea92d1193f8
Day <daychahk@gmail.com>
2018-10-30 10:47:47 +0000

    mainloop test

--- detection_loop_multiThread/zmq_send/zmq_send.py
+++ detection_loop_multiThread/zmq_send/zmq_send.py
@@ -7,11 +7,13 @@ import time
 import pickle
 context = zmq.Context()  
 socket = context.socket(zmq.PUB)  
-socket.bind("tcp://0.0.0.0:5000")
+socket.bind("tcp://0.0.0.0:6666")
 print("Port 5000 connect well")  
 msg = cv2.imread('picture.jpg')
 while True:  
+    start = time.time()
     msg_pickle = pickle.dumps(msg)
     socket.send(msg_pickle)
     print("Send successfully")
-    time.sleep(0.2)
+    time.sleep(0.1)
+    print("Send Time: ",time.time()-start)
